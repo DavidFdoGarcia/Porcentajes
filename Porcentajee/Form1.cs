1b16961babe6ec44caa37073247f3a061138bcdd
David Garcia <davidfdo.garcia@gmail.com>
2022-09-07 14:04:22 -0500

    primer commit

---
+++ Porcentajee/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Porcentajee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txConvertir_Click(object sender, EventArgs e)
+        {
+            double x, d;
+
+            x = double.Parse(textBox1.Text);
+
+            d = 0.2 * x;
+
+            MessageBox.Show("El 20% de " + x + " es: " + d);
+        }
+
+        private void txConvertir_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           // validacion.SoloNumeros(e);
+            if (e.KeyChar == 13)
+            {
+                double x, d;
+
+                x = double.Parse(textBox1.Text);
+
+                d = 0.2 * x;
+
+                MessageBox.Show("El 20% de " + x + " es: " + d);
+            }
+        }
+    }
+}
---
+++ Porcentajee/validacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Porcentajee
+{
+    class validacion
+    {
+        public static void SoloNumeros(KeyPressEventArgs v)
+        {
+            if (Char.IsDigit(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+                MessageBox.Show("Solo Numeros");
+            }
+        }
+    }
+}
